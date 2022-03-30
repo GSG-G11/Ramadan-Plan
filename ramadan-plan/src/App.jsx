bc9a2779e0998c8d09bca02c0513f72142e7df51
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-30 16:04:33 +0300

    Create App Component with style Relates #2

---
+++ ramadan-plan/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+console.log();
+function App() {
+  return (
+    <div className="main-section">
+      <h1 className="title">RamadanPlan</h1>
+      <p className="description">save your plan here..</p>
+    </div>
+  );
+}
+
+export default App;
