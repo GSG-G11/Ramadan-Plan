d0b7285e28bd39f45b1fa1d7d1b6eaa3ea433acd
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-30 19:03:05 +0300

    Finish textarea handling with style Relates #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import { Component } from 'react';
 import './App.css';
-console.log();
+import TextAreaComponent from './Components/TextAreaComponent';
 class App extends Component {
-  state = {};
-  
+  state = {
+    text: '',
+    isTextAdded: false,
+    isTextMoreThanFive: false,
+  };
+  handleOnTextAdded = () => {
+    const { text } = this.state;
+    this.setState({
+      isTextAdded: text.length >= 5 ? true : false,
+      text: '',
+    });
+  };
+  handleOnTextChanged = (value) => {
+    this.setState({
+      text: value,
+    });
+  };
+
   render() {
+    const { text, isTextAdded } = this.state;
+    console.log(text, isTextAdded);
     return (
-      <div className="main-section">
-        <h1 className="title">RamadanPlan</h1>
-        <p className="description">save your plan here..</p>
-      </div>
+      <>
+        <div className="main-section">
+          <h1 className="title">RamadanPlan</h1>
+          <p className="description">save your plan here...</p>
+        </div>
+        <div className="form-section">
+          <form className="form">
+            <TextAreaComponent
+              handleOnTextAdded={this.handleOnTextAdded}
+              handleOnTextChanged={this.handleOnTextChanged}
+              text={text}
+              isTextAdded={isTextAdded}
+            />
+          </form>
+        </div>
+      </>
     );
   }
 }
