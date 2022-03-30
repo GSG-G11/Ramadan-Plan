8c3d2a04d29ba0a1354e876ea0d6be863e174aa4
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-30 23:02:24 +0300

    handle add plan Relates #5

---
+++ src/Components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Card({ data, isTextAdded }) {
+  return (
+    <div className="cards-container">
+      {data.map((plan, index) => {
+        return (
+          <div className="card" key={index}>
+            <p className="published-at">
+              Published At: {`${new Date().toJSON().slice(0, 10)}`}
+            </p>
+            <p className="date">{plan.addedDay} / Ramadan</p>
+            <p className="text">{plan.addedText}</p>
+            <div className="icons">
+              <i className="icon fa fa-trash"></i>
+              <i className="icon fas fa-edit"></i>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Card;
